ae0521a96b147b5a9ff4f71cb90d8fc0174a424d
Datseris <datseris.george@gmail.com>
2018-01-16 19:52:46 +0100

    export columns loooool

--- src/dataset.jl
+++ src/dataset.jl
@@ -3,7 +3,7 @@ using IterTools: chain
 import Base: ==
 
 export Dataset, AbstractDataset, minima, maxima
-export read_dataset, write_dataset, minmaxima
+export read_dataset, write_dataset, minmaxima, columns
 
 abstract type AbstractDataset{D, T} end
 
