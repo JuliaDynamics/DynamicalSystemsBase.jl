f8edfb41f701d9775b8fe568c0631079d0f223c7
Datseris <datseris.george@gmail.com>
2018-01-26 21:58:00 +0100

    better warning message

--- deps/build.jl
+++ deps/build.jl
@@ -1,10 +1,18 @@
 infomsg = """
 ~~~ DynamicalSystemsBase.jl update message ~~~
 
-We have changed the fundamental way users define equations of motion and Jacobians
-for all subtypes of `DynamicalSystem`! In short, parameters are now directly passed
-into the equations of motion function, e.g. `f(du, u, p, t)`. These updates are in-line
+We have changed the fundamental way users define
+equations of motion and Jacobians
+for all subtypes of `DynamicalSystem`!
+In short, parameters are now directly passed
+into the equations of motion function, e.g.
+`f(du, u, p, t)`. These updates are in-line
 with DifferentialEquations v4.0 release.
+
+Notice that the same changes happened for
+discrete systems as well. For example, the
+e.o.m. for `DiscreteDS` are now expected
+in the form `eom(x, p)`.
 
 Please see the documentation strings of the system types you are using!
 
