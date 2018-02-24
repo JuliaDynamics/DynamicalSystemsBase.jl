7b70018c2a15a0145a352880fc3c5e3469392702
Allen Hill <allenofthehills@gmail.com>
2018-02-24 14:25:25 -0500

    Add tests for SizedArray Reconstructions

--- test/reconstruction_tests.jl
+++ test/reconstruction_tests.jl
@@ -15,4 +15,16 @@ using Base.Test, StaticArrays
 		@test R[(1+τ):end, 1] == R[1:end-τ, 2]
 		@test size(R) == (length(s) - τ*(D - 1), D)
 	end
+
+    @testset "D = $(D), τ = $(τ), basedim = $(basedim)" for D in [2,3], τ in [2,3], basedim in [2,3]
+
+        si = Matrix(data[:,1:basedim])
+        s = Size(10001,basedim)(si)
+        R = Reconstruction(s, D, τ)
+
+        for dim in 1:basedim
+            @test R[(1+τ):end, dim] == R[1:end-τ, dim+basedim]
+        end
+        @test size(R) == (size(s,1) - τ*(D - 1), D*basedim)
+    end
 end
