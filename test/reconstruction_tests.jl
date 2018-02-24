849f735ab254acb845bf88ca65b62b15b966b972
Allen Hill <allenofthehills@gmail.com>
2018-02-24 14:07:14 -0500

    Fix indentation

--- test/reconstruction_tests.jl
+++ test/reconstruction_tests.jl
@@ -5,14 +5,14 @@ end
 using Base.Test, StaticArrays
 
 @testset "Reconstruction" begin
-  ds = Systems.towel()
-  data = trajectory(ds, 10000)
-  s = data[:, 1]; N = length(s)
-  @testset "D = $(D), τ = $(τ)" for D in [2,3], τ in [2,3]
+	ds = Systems.towel()
+	data = trajectory(ds, 10000)
+	s = data[:, 1]; N = length(s)
+	@testset "D = $(D), τ = $(τ)" for D in [2,3], τ in [2,3]
 
-    R = Reconstruction(s, D, τ)
+		R = Reconstruction(s, D, τ)
 
-    @test R[(1+τ):end, 1] == R[1:end-τ, 2]
-    @test size(R) == (length(s) - τ*(D - 1), D)
- end
+		@test R[(1+τ):end, 1] == R[1:end-τ, 2]
+		@test size(R) == (length(s) - τ*(D - 1), D)
+	end
 end
