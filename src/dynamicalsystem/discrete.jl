df798b3c613dcd15b6b4106020d7e88c45af734b
George Datseris <datseris.george@gmail.com>
2018-05-03 11:09:28 +0200

    reinit add

--- src/dynamicalsystem/discrete.jl
+++ src/dynamicalsystem/discrete.jl
@@ -118,10 +118,13 @@ function init(prob::MDP{IIP, S, D, F, P}, u0 = prob.u0) where {IIP, S, D, F, P}
     return MDI{IIP, S, D, F, P}(prob.f, S(u0), prob.t0, S(deepcopy(u0)), prob.p, prob.t0)
 end
 
-function reinit!(integ::MDI, u; t0 = integ.t0)
+function reinit!(integ::MDI, u = integ.u; t0 = integ.t0, Q0 = nothing)
     integ.u = u
     integ.dummy = u
     integ.t = t0
+    if Q0 != nothing
+        set_deviations!(integ, Q0)
+    end
     return
 end
 
@@ -218,6 +221,17 @@ u_modified!(t::TDI, a) = nothing
 
 get_deviations(t::TDI) = t.W
 set_deviations!(t::TDI, Q) = (t.W = Q)
+
+function reinit!(integ::TDI, u = integ.u;
+    t0 = integ.t0, Q0 = nothing)
+    set_state!(integ, u)
+    if Q0 != nothing
+        set_deviations!(integ, Q0)
+    end
+    integ.t = t0
+    return
+end
+
 
 #####################################################################################
 #                                 Tangent Stepping                                  #
