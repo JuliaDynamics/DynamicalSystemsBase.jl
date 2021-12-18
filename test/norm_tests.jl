aae6eadfd5ef9f1a2399e12f155dd2ca3653063e
Datseris <datseris.george@gmail.com>
2021-12-18 12:37:46 +0100

    increase stepping size in internal norm tests

--- test/norm_tests.jl
+++ test/norm_tests.jl
@@ -8,13 +8,15 @@ println("\nTesting internal norms...")
 
 
 # %%
+@testset begin "internal norm: tangent"
 i1 = tangent_integrator(ds, 3; diffeq = (alg = ALG,))
 i2 = tangent_integrator(ds, 3; diffeq = (alg = ALG, internalnorm = DynamicalSystemsBase._standardnorm))
 
 step!(i1)
 step!(i2)
-dts = zeros(2, 500)
-for i in 1:500
+N = 1000
+dts = zeros(2, N)
+for i in 1:N
     step!(i1); step!(i2)
     dts[:, i] .= (i1.dt, i2.dt)
 end
@@ -23,8 +25,9 @@ dt1 = mean(dts[1, :])
 dt2 = mean(dts[2, :])
 
 @test dt1 > dt2
-
+end
 # %%
+@testset begin "internal norm: parallel"
 s = [get_state(ds), get_state(ds) .+ rand(3)]
 i1 = parallel_integrator(ds, deepcopy(s);
 diffeq = (alg = ALG, internalnorm = DynamicalSystemsBase._standardnorm))
@@ -32,8 +35,10 @@ i2 = parallel_integrator(ds, deepcopy(s); diffeq = (alg = ALG,))
 
 step!(i1)
 step!(i2)
-dts = zeros(2, 1000)
-for i in 1:1000
+
+N = 2000
+dts = zeros(2, N)
+for i in 1:N
     step!(i1); step!(i2)
     dts[:, i] .= (i1.dt, i2.dt)
 end
@@ -42,3 +47,4 @@ dt1 = mean(dts[1, :])
 dt2 = mean(dts[2, :])
 
 @test dt2 > dt1
+end
