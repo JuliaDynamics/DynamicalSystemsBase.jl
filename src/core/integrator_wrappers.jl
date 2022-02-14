67175c930f0dc983be4573fe9a45457f513eea35
awage <>
2022-02-14 20:55:05 +0100

    Include CompleteAndReinit in DynamicalSystemsBase

--- src/core/integrator_wrappers.jl
+++ src/core/integrator_wrappers.jl
@@ -152,3 +152,44 @@ function Base.show(io::IO, psys::ProjectedSystem)
     println(io,  rpad(" rule f: ", 14),     DynamicalSystemsBase.eomstring(psys.integ.f.f))
     #println(io,  rpad(" Period: ", 14),     psys.T)
 end
+
+
+
+
+
+# Utilities for re-initializing initial conditions on the grid
+"""
+    CompleteAndReinit(complete_state, idxs, D)
+Helper struct that completes a state and reinitializes the integrator once called
+as a function with arguments `f(integ, y)` with `integ` the initialized dynamical
+system integrator and `y` the projected initial condition on the grid.
+"""
+struct CompleteAndReinit{C, Y, R}
+    complete_state::C
+    u::Vector{Float64} # dummy variable for a state in full state space
+    idxs::SVector{Y, Int}
+    remidxs::R
+end
+function CompleteAndReinit(complete_state, idxs, D::Int)
+    remidxs = setdiff(1:D, idxs)
+    remidxs = isempty(remidxs) ? nothing : SVector(remidxs...)
+    u = zeros(D)
+    if complete_state isa AbstractVector
+        @assert eltype(complete_state) <: Number
+    end
+    return CompleteAndReinit(complete_state, u, idxs, remidxs)
+end
+function (c::CompleteAndReinit{<: AbstractVector})(integ, y)
+    c.u[c.idxs] .= y
+    if !isnothing(c.remidxs)
+        c.u[c.remidxs] .= c.complete_state
+    end
+    reinit!(integ, c.u)
+end
+function (c::CompleteAndReinit)(integ, y) # case where `complete_state` is a function
+    c.u[c.idxs] .= y
+    if !isnothing(c.remidxs)
+        c.u[c.remidxs] .= c.complete_state(y)
+    end
+    reinit!(integ, c.u)
+end
