5a2b67ac1963b5a7f09cc27a3ce0503995220810
George Datseris <datseris.george@gmail.com>
2022-04-09 23:27:03 +0200

    new function `get_states` (#143)
    * new function `get_states`
    
    * also add test
    
    * fix location of get_states

--- src/DynamicalSystemsBase.jl
+++ src/DynamicalSystemsBase.jl
@@ -28,7 +28,7 @@ include("predefined/famous_systems.jl") # includes both discrete and continuous
 export GeneralizedDynamicalSystem, DynamicalSystem
 export ContinuousDynamicalSystem, DiscreteDynamicalSystem
 export get_rule_for_print, isdiscretetime
-export dimension, get_state
+export dimension, get_state, get_states
 export set_parameter!, trajectory, jacobian
 export integrator, tangent_integrator, parallel_integrator
 export set_state!, get_state, get_deviations, set_deviations!, current_time
