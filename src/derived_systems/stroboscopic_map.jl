97f8751fbf251704df46d288ff33d45b871496b9
Datseris <datseris.george@gmail.com>
2023-01-30 16:47:16 +0000

    duffing chaotic trajectory works

--- src/derived_systems/stroboscopic_map.jl
+++ src/derived_systems/stroboscopic_map.jl
@@ -5,23 +5,31 @@ export StroboscopicMap
 ##################################################################################
 """
 	StroboscopicMap(ds::CoupledODEs, T::Real) <: DynamicalSystem
-	StroboscopicMap(T::Real, f, u0, p = nothing; diffeq, t0 = nothing)
+	StroboscopicMap(T::Real, f, u0, p = nothing; diffeq, t0 = 0)
 
-A discrete time dynamical system that produces iterations of a time-dependent
+A discrete time autonomous dynamical system that produces iterations of a time-dependent
 (non-autonomous) [`CoupledODEs`](@ref) system exactly over a period `T`.
 This is known as a stroboscopic map.
-The second signature creates a [`CoupledODEs`](@ref) and calls the first.
+The second signature creates a [`CoupledODEs`](@ref) and calls the first
+(with `t0` measured in continuous time).
 
-See also [`poincaremap`](@ref).
+As this system is in discrete time, [`current_time`](@Ref) and [`initial_time`](@ref)
+are integers. The initial time is always 0, because `current_time` counts elapsed periods.
+Call these functions on the field `.integ` of `StroboscopicMap` to obtain the
+corresponding continuous time.
+In contrast, [`reinit!`](@ref) expects `t0` in continuous time.
+
+See also [`PoincareMap`](@ref).
 """
 struct StroboscopicMap{D, I, P, TT<:Real} <: DiscreteTimeDynamicalSystem
 	integ::I
 	p0::P
 	T::TT
+	t::Base.RefValue{Int}
 end
 
 StroboscopicMap(ds::CoupledODEs{D, I, P}, T::TT) where {D, I, P, TT} =
-StroboscopicMap{D, I, P, TT}(ds.integ, ds.p0, T)
+StroboscopicMap{D, I, P, TT}(ds.integ, ds.p0, T, Ref(0))
 
 StroboscopicMap(T, f, u0, p = nothing; kwargs...) =
 StroboscopicMap(CoupledODEs(f, u0, p; kwargs...), T)
@@ -35,20 +43,25 @@ for f in (:current_state, :initial_state, :current_parameters, :dynamic_rule,
 end
 SciMLBase.isinplace(ds::StroboscopicMap) = isinplace(ds.integ.f)
 StateSpaceSets.dimension(::StroboscopicMap{D}) where {D} = D
+current_time(smap::StroboscopicMap) = smap.t[]
+initial_time(smap::StroboscopicMap) = 0
 
 function SciMLBase.step!(smap::StroboscopicMap)
 	step!(smap.integ, smap.T, true)
+	smap.t[] = smap.t[] + 1
 	return
 end
 function SciMLBase.step!(smap::StroboscopicMap, n::Int)
 	for _ in 1:n; step!(smap.integ, smap.T, true); end
+	smap.t[] = smap.t[] + n
 	return
 end
 
 function SciMLBase.reinit!(ds::StroboscopicMap, u = initial_state(ds);
-		p0 = current_parameters(ds), t0 = initial_time(ds)
+		p0 = current_parameters(ds), t0 = initial_time(ds.integ)
 	)
 	isnothing(u) && return
 	set_parameters!(ds, p0)
+	ds.t[] = 0
 	reinit!(ds.integ, u; reset_dt = true, t0)
 end
--- test/new_tests/stroboscopic.jl
+++ test/new_tests/stroboscopic.jl
@@ -16,8 +16,8 @@ function duffing_rule_iip(du, u, p, t)
 end
 
 u0 = [0.1, 0.25]
-p0 = [2.2, 27.0, 0.2, 1]
-T = 2π/ω
+p0 = [1.0, 0.3, 0.2, -1]
+T = 2π/1.0
 
 duffing_oop = StroboscopicMap(CoupledODEs(duffing_rule, u0, p0), T)
 duffing_iip = StroboscopicMap(T, duffing_rule_iip, copy(u0), p0)
@@ -28,7 +28,15 @@ duffing_vern = StroboscopicMap(T, duffing_rule, u0, p0;
 for (ds, iip) in zip((duffing_oop, duffing_iip, duffing_vern), (false, true, false))
 
     @test dynamic_rule(ds) == (iip ? duffing_rule_iip : duffing_rule)
-    name = (ds === duffing_vern) ? "lorvern" : "duffing"
+    name = (ds === duffing_vern) ? "duffvern" : "duffing"
     test_dynamical_system(ds, u0, name, false, iip)
 
 end
+
+# TODO: Add extra test that duffing goes to fixed point
+# with
+# ω = 2.2, f = 27.0, d = 0.2, β = 1
+
+# TODO: reproduce this video, to ensure `reinit!` works as expected:
+# https://juliadynamics.github.io/DynamicalSystems.jl/dev/chaos/orbitdiagram/#Stroboscopic-Map
+# using `reinit!`
