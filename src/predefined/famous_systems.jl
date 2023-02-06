ac451beaccaa1a05d0d131888acc68bb6c2fafd0
Datseris <datseris.george@gmail.com>
2023-02-06 12:36:17 +0000

    establish a naming convention for hte Jacobian function

--- src/predefined/famous_systems.jl
+++ src/predefined/famous_systems.jl
@@ -1,7 +1,17 @@
 """
-Sub-module of the module `DynamicalSystemsBase`, which contains pre-defined
+Submodule of the module `DynamicalSystemsBase`, which contains pre-defined
 famous systems. This is provided purely as a convenience.
 Nothing here is tested, nor guaranteed to be stable in future versions.
+
+Predefined systems exist in the `Systems` submodule in the form of functions that
+return a `DynamicalSystem`. They are accessed like:
+```julia
+ds = Systems.lorenz(u0; ρ = 32.0)
+```
+
+For some systems, a Jacobian function is also defined.
+The naming convention for the jacobian function is `\$(name)_jacob`.
+So, for the above example we have `J = Systems.lorenz_jacob`.
 """
 module Systems
 using DynamicalSystemsBase
