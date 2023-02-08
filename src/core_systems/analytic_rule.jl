28823d8c74e20197b8e7297e2f15d26f10462f2c
Datseris <datseris.george@gmail.com>
2023-02-08 09:56:45 +0000

    comments on analytic rule system

--- src/core_systems/analytic_rule.jl
+++ src/core_systems/analytic_rule.jl
@@ -3,6 +3,10 @@ export AnalyticRuleSystem
 """
     AnalyticRuleSystem
 
-Abstract type meaning either [`DeterministicIteratedMap`](@ref) or [`CoupledODEs`](@ref).
+Abstract type meaning either [`DeterministicIteratedMap`](@ref) or [`CoupledODEs`](@ref),
+which are the systems whose dynamic rule `f` is known analytically.
+
+This type is used for deciding whether a creation of a [`TangentDynamicalSystem`](@ref)
+is possible or not.
 """
 AnalyticRuleSystem{IIP} = Union{CoupledODEs{IIP}, DeterministicIteratedMap{IIP}}
