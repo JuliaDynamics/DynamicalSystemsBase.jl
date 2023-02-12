52eb73d83d13047acd1a3f06247cb4ba8ee2e8c2
Datseris <datseris.george@gmail.com>
2023-02-12 14:51:20 +0000

    first draft of arbitrary steppable

--- src/DynamicalSystemsBase.jl
+++ src/DynamicalSystemsBase.jl
@@ -22,7 +22,7 @@ include("core/pretty_printing.jl")
 
 include("core_systems/discrete_time_map.jl")
 include("core_systems/continuous_time_ode.jl")
-include("core_systems/arbitrary_steppable.jl")
+# include("core_systems/arbitrary_steppable.jl")
 include("core_systems/additional_supertypes.jl")
 
 include("derived_systems/stroboscopic_map.jl")
