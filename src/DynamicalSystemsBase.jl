6696c15385f6aa21db4aa25c5ff71ab5e6224780
Datseris <datseris.george@gmail.com>
2023-02-12 14:29:45 +0000

    remove predefined systemsi n favor of PredefinedDynamicalSystems.jl

--- src/DynamicalSystemsBase.jl
+++ src/DynamicalSystemsBase.jl
@@ -36,29 +36,4 @@ include("predefined/famous_systems.jl")
 
 include("deprecations.jl")
 
-# include("core/dynamicalsystem.jl")
-# include("core/create_dynamics.jl")
-# include("core/api_docstrings.jl")
-# include("core/discrete.jl")
-# include("core/continuous.jl")
-# include("core/pretty_printing.jl")
-
-# include("advanced_integrators/projected_integrator.jl")
-# include("advanced_integrators/stroboscopic_map.jl")
-# include("advanced_integrators/poincare.jl")
-
-# include("predefined/famous_systems.jl") # includes both discrete and continuous
-
-# export GeneralizedDynamicalSystem, DynamicalSystem
-# export ContinuousDynamicalSystem, DiscreteDynamicalSystem
-# export rulestring, isdiscretetime
-# export dimension, get_state, get_states
-# export get_parameter, get_parameters, set_parameter!
-# export trajectory, jacobian
-# export integrator, tangent_integrator, parallel_integrator
-# export set_state!, get_state, get_deviations, set_deviations!, current_time
-# export SciMLBase, init, step!, isinplace, reinit!
-# export Systems
-# export projected_integrator, stroboscopicmap
-
 end
