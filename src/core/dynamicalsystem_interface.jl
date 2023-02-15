911a2b68354f507ac015202a97028fae0be8775c
Datseris <datseris.george@gmail.com>
2023-02-15 11:59:33 +0000

    finish source of arbitrary steppable

--- src/core/dynamicalsystem_interface.jl
+++ src/core/dynamicalsystem_interface.jl
@@ -294,15 +294,15 @@ end
 # API - step and reset
 ###########################################################################################
 """
-    step!(ds::DiscreteTimeDynamicalSystem [, dt::Integer]) → ds
+    step!(ds::DiscreteTimeDynamicalSystem [, n::Integer]) → ds
 
-Evolve the discrete time dynamical system for 1 or `dt` steps.
+Evolve the discrete time dynamical system for 1 or `n` steps.
 
     step!(ds::ContinuousTimeDynamicalSystem, [, dt::Real [, stop_at_tdt]]) → ds
 
 Evolve the continuous time dynamical system for one integration step.
 
-Alternative, if a `dt` is given, then progress the integration until
+Alternatively, if a `dt` is given, then progress the integration until
 there is a temporal difference `≥ dt` (so, step _at least_ for `dt` time).
 
 When `true` is passed to the optional third argument,
