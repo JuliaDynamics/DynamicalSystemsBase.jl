0cca8d940e8687b5a81fd6d8c7187418f4f8b2fe
Datseris <datseris.george@gmail.com>
2024-04-11 07:45:25 +0100

    improve docstring of set _parametrs, allow pair

--- src/core/dynamicalsystem_interface.jl
+++ src/core/dynamicalsystem_interface.jl
@@ -420,14 +420,18 @@ end
 """
     set_parameters!(ds::DynamicalSystem, p = initial_parameters(ds))
 
-Set the parameter values in the [`current_parameters`](@ref)`(ds)` to match `p`.
-This is done as an in-place overwrite by looping over the keys of `p`.
-Hence the keys of `p` must be a subset of the keys of [`current_parameters`](@ref)`(ds)`.
+Set the parameter values in the [`current_parameters`](@ref)`(ds)` to match those in `p`.
+This is done as an in-place overwrite by looping over the keys of `p`
+hence `p` can be an arbitrary container mapping parameter indices to values
+(such as a `Vector`, `Vector{Pair}`, or `Dict`).
+
+The keys of `p` must be valid keys that can be given to [`set_parameter!`](@ref).
 """
 function set_parameters!(ds::DynamicalSystem, p = initial_parameters(ds))
     cp = current_parameters(ds)
     p === cp && return
-    for (index, value) in pairs(p)
+    iter = p isa Vector ? pairs(p) : p # allows using vector, dict, or vector{pair}.
+    for (index, value) in iter
         _set_parameter!(ds, index, value, cp)
     end
     return
