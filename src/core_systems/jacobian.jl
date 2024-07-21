ac6f54bce3303eda3e8e0a75d349379223162eb4
Jonas Koziorek <jonas.koziorek@gmail.com>
2024-07-21 15:13:43 +0200

    docstring

--- src/core_systems/jacobian.jl
+++ src/core_systems/jacobian.jl
@@ -2,6 +2,22 @@ export jacobian
 
 import ForwardDiff
 
+"""
+    jacobian(ds::CoreDynamicalSystem)
+
+Construct the Jacobian rule for the dynamical system `ds`.
+This is done via automatic differentiation using module 
+[`ForwardDiff`](https://github.com/JuliaDiff/ForwardDiff.jl).
+
+## Description
+
+For out-of-place systems, `jacobian` returns the Jacobian rule as a 
+function `Jf(u, p, t) -> J0::SMatrix`. Calling `Jf(u, p, t)` will compute the Jacobian 
+at the state `u`, parameters `p` and time `t` and return the result as `J0`.
+For in-place systems, `jacobian` returns the Jacobian rule as a function 
+`Jf!(J0, u, p, t)`. Calling `Jf!(J0, u, p, t)` will compute the Jacobian 
+at the state `u`, parameters `p` and time `t` and save the result in `J0`.
+"""
 function jacobian(ds::CoreDynamicalSystem{IIP}) where {IIP}
     _jacobian(ds, Val{IIP}())
 end
@@ -12,7 +28,7 @@ function _jacobian(ds, ::Val{true})
     cfg = ForwardDiff.JacobianConfig(
         (du, u) -> f(du, u, p, p), deepcopy(u0), deepcopy(u0)
     )
-    Jf = (J0, u, p, t) -> begin
+    Jf! = (J0, u, p, t) -> begin
         uv = @view u[:, 1]
         du = copy(u)
         ForwardDiff.jacobian!(
@@ -20,7 +36,7 @@ function _jacobian(ds, ::Val{true})
         )
         nothing
     end
-    return Jf
+    return Jf!
 end
 
 function _jacobian(ds, ::Val{false})
