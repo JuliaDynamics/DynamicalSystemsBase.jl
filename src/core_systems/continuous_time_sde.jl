62c5e7ec4587d37981b6f436b80ce5a4850034df
oameye <orjan.ameye@hotmail.com>
2024-07-28 11:08:39 +0200

    apply suggested format changes

--- src/core_systems/continuous_time_sde.jl
+++ src/core_systems/continuous_time_sde.jl
@@ -21,8 +21,6 @@ end
 ###########################################################################################
 # Type
 ###########################################################################################
-
-# Define CoupledSDEs
 """
     CoupledSDEs <: ContinuousTimeDynamicalSystem
     CoupledSDEs(f, g, u0 [, p, σ]; kwargs...)
@@ -30,14 +28,24 @@ end
 A stochastic continuous time dynamical system defined by a set of
 coupled ordinary differential equations as follows:
 ```math
-d\\vec{u} = \\vec{f}(\\vec{u}, p, t) dt + \\vec{g}(\\vec{u}, p, t) dW_t
+d\\vec{u} = \\vec{f}(\\vec{u}, p, t) dt + σ \\vec{g}(\\vec{u}, p, t) dW_t
 ```
 
-Optionally provide the overall noise strength `σ`, the parameter container `p` and initial time as keyword `t0`. If `σ` is provided, the diffusion function `g` is multiplied by `σ`.
+Optionally provide the overall noise strength `σ`, the parameter container `p`
+and initial time as keyword `t0`. By defualt `σ=1` and `t0=0`.
 
-For construction instructions regarding `f, u0` see the [DynamicalSystems.jl tutorial](https://juliadynamics.github.io/DynamicalSystems.jl/latest/tutorial/#CoupledODEs).
+For construction instructions regarding `f, u0` see the [DynamicalSystems.jl tutorial
+](https://juliadynamics.github.io/DynamicalSystemsDocs.jl/dynamicalsystems/dev/tutorial/).
 
-The stochastic part of the differential equation is defined by the function `g` and the keyword arguments `noise_rate_prototype` and `noise`. `noise` indicates the noise process applied during generation and defaults to Gaussian white noise. For details on defining various noise processes, refer to the noise process documentation page. `noise_rate_prototype` indicates the prototype type instance for the noise rates, i.e., the output of `g`. It can be any type which overloads `A_mul_B!` with itself being the middle argument. Commonly, this is a matrix or sparse matrix. If this is not given, it defaults to `nothing`, which means the problem should be interpreted as having diagonal noise.
+The stochastic part of the differential equation is defined by the function `g` and
+the keyword arguments `noise_rate_prototype` and `noise`. `noise` indicates
+the noise process applied and defaults to Gaussian white noise (Wiener process).
+For details on defining various noise processes, refer to [DiffEqNoiseProcess.jl
+](https://docs.sciml.ai/DiffEqNoiseProcess/stable/).
+`noise_rate_prototype` indicates the prototype type instance for the noise rates, i.e.,
+the output of `g`. It can be any type which overloads `A_mul_B!` with itself being the
+middle argument. Commonly, this is a matrix or sparse matrix. If this is not given, it
+defaults to `nothing`, which means the `g` should be interpreted as being diagonal.
 
 ## DifferentialEquations.jl interfacing
 
@@ -56,7 +64,7 @@ $(DEFAULT_STOCH_DIFFEQ)
 `diffeq` keywords can also include `callback` for [event handling
 ](https://docs.sciml.ai/DiffEqDocs/stable/features/callback_functions/).
 
-Dev note: `CoupledSDEs` is a light wrapper of  `StochasticDiffEq.SDEIntegrator` from DifferentialEquations.jl.
+Dev note: `CoupledSDEs` is a light wrapper of  `SDEIntegrator` from StochasticDiffEq.jl.
 The integrator is available as the field `integ`, and the `SDEProblem` is `integ.sol.prob`.
 The convenience syntax `SDEProblem(ds::CoupledSDEs, tspan = (t0, Inf))` is available
 to extract the problem.
@@ -103,9 +111,13 @@ end
 
 function CoupledSDEs(
     prob::SDEProblem, diffeq=DEFAULT_STOCH_DIFFEQ, noise_strength=1.0; special_kwargs...
-    )
+)
     if haskey(special_kwargs, :diffeq)
-        throw(ArgumentError("`diffeq` is given as positional argument when an ODEProblem is provided."))
+        throw(
+            ArgumentError(
+                "`diffeq` is given as positional argument when an ODEProblem is provided."
+            ),
+        )
     end
     IIP = isinplace(prob) # from SciMLBase
     D = length(prob.u0)
@@ -139,7 +151,8 @@ end
 """
     CoupledSDEs(ds::CoupledODEs, g, p [, σ]; kwargs...)
 
-Converts a [`CoupledODEs`](https://juliadynamics.github.io/DynamicalSystems.jl/stable/tutorial/#CoupledODEs)
+Converts a [`CoupledODEs`
+](https://juliadynamics.github.io/DynamicalSystems.jl/stable/tutorial/#CoupledODEs)
 system into a [`CoupledSDEs`](@ref).
 """
 function CoupledSDEs(
@@ -170,9 +183,7 @@ end
 
 Converts a [`CoupledSDEs`](@ref) into [`CoupledODEs`](@ref).
 """
-function CoupledODEs(
-    sys::CoupledSDEs; diffeq=DEFAULT_STOCH_DIFFEQ, t0=0.0
-)
+function CoupledODEs(sys::CoupledSDEs; diffeq=DEFAULT_STOCH_DIFFEQ, t0=0.0)
     return CoupledODEs(
         sys.integ.f, SVector{length(sys.integ.u)}(sys.integ.u), sys.p0; diffeq=diffeq, t0=t0
     )
