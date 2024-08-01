dfc3053ea20f69ad1a2b0d85b71670b8ef21d490
oameye <orjan.ameye@hotmail.com>
2024-08-01 09:10:43 +0200

    add extention to modules in makedocs

--- docs/make.jl
+++ docs/make.jl
@@ -2,6 +2,7 @@ cd(@__DIR__)
 
 using DynamicalSystemsBase
 using StochasticDiffEq # for extention
+StochasticSystemsBase = Base.get_extension(DynamicalSystemsBase, :StochasticSystemsBase)
 
 pages = [
     "index.md",
@@ -17,5 +18,5 @@ Downloads.download(
 include("build_docs_with_style.jl")
 
 build_docs_with_style(pages,
-    DynamicalSystemsBase, SciMLBase, StateSpaceSets, StochasticDiffEq;
+    DynamicalSystemsBase, SciMLBase, StateSpaceSets, StochasticSystemsBase;
 )
